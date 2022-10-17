5b34c59e2ab7eb078295a7973c76c84d4e562ca4
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-10-17 20:00:29 -0300

    teste azure

--- scrip/conect.py
+++ scrip/conect.py
@@ -3,26 +3,24 @@ import textwrap
 
 #Definindo o driver
 driver = '{ODBC Driver 17 for SQL Server}'
-
-# definindo server 
-server_name = 'monitoll'
+# definindo server e database
+server_name = 'montioll'
 database_name = 'Monitoll'
-
 server = '{server_name}.database.windows.net,1433'.format(server_name=server_name)
 # definindo o nome e a senha 
 username = 'Monitoll'
-password = 'Monitoll123'
+password = 'Grupo7@123'
 
 # definindo o banco url
 connection_string = textwrap.dedent("""
-Driver ={driver};
-Server={server};
-Database={database};
-Uid={username};
-Pwd={password};
-Encrypt=yes;
-TrustedServerCertificate=no;
-Connection Timeout=30;
+    Driver={driver};
+    Server={server};
+    Database={database};
+    Uid={username};
+    Pwd={password};
+    Encrypt=yes;
+    TrustedServerCertificate=no;
+    Connection Timeout=10;
 """.format(
     driver=driver,
     server=server,
@@ -34,23 +32,28 @@ Connection Timeout=30;
 # criando o objeto de conecxao
 cnxn:pyodbc.Connection = pyodbc.connect(connection_string)
 
-#criando execute vdd
-crsr.fast_executemany= True
 
 # criando um novo cursor 
-crsr: pyodbc.cursor = cnxn.cursor()
+crsr: pyodbc.Cursor = cnxn.cursor()
 
+#criando execute vdd
+crsr.fast_executemany= True
 # definindo insert query 
-insert_query = "INSERT INTO [Torre] (SerialID, SO, Maquina, Processador, Disco, VelocidadeRam, fkEmpresa) VALUES(?,?,?,?,?,?,?)"
-records=[] 
+insert_sql = "INSERT INTO [Perfil] (idPerfil,Cargo) VALUES(?,?)" 
 
+# criando record 
+records = [('4','Dono')]
+
+# select 
+# select_sql = " SELECT * FROM [PERFIL]"
 # definindo 
 
 #executando insert 
-crsr.executemany(insert_query, records)
+crsr.executemany(insert_sql, records)
 
 # commit 
 crsr.commit()
+
 
 # close the connection
 cnxn.close()

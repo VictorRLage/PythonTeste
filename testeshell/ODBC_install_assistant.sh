db4d968febd95c3f023ffb1517b5d6535f625498
pedrosptech2022 <pedro.cordeironeto@sptech.school>
2022-10-29 23:06:38 -0300

    add at

--- testeshell/ODBC_install_assistant.sh
+++ testeshell/ODBC_install_assistant.sh
@@ -21,7 +21,6 @@ if [ apt-cache search msodbc -eq 0 ]
 			sudo su
             curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
             curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
-            exit
             sudo apt-get update
             sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
             sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
